73803d47b71c92556e829182d30cd2be4b9e0ffd
Md Arif <aatif149209@gmail.com>
2025-02-08 01:09:08 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
 
 // const serviceAccount = require("./firebase-admin.json");
-const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+// const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 const cron = require("node-cron");
 
@@ -31,7 +31,19 @@ app.use("/api/reminders", reminderRoutes);
 //   credential: admin.credential.cert(require("./firebase-admin.json")),
 // });
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key.replace(/\\n/g, "\n"), // Ensure newlines are handled
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: process.env.universe_domain,
+  }),
 });
 
 //to send notifications manually
