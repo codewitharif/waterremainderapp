6f3afd43b924e3032a594d8ab2a96f803b692324
Md Arif <aatif149209@gmail.com>
2025-02-11 13:18:01 +0530

    Fix FCM notification click action for web app

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAUk1BHyGcRhO7eqU5ImoSJY-j0brvoehY",
   authDomain: "water-reminder-app-b47b5.firebaseapp.com",
   projectId: "water-reminder-app-b47b5",
-  storageBucket: "water-reminder-app-b47b5.firebasestorage.app",
+  storageBucket: "water-reminder-app-b47b5.appspot.com",
   messagingSenderId: "289129177395",
   appId: "1:289129177395:web:d97c4a7d47155f4e8e1e46",
 };
