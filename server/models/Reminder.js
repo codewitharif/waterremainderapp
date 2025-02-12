842ea04cdce432d56846763a5195ab88a855258e
Md Arif <aatif149209@gmail.com>
2025-02-12 10:30:12 +0530

    Fix model

--- server/models/Reminder.js
+++ server/models/Reminder.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ReminderSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  time: { type: String, required: true },
+  time: { type: String, required: true, match: /^\d{2}:\d{2}$/ }, // Ensures proper HH:MM format
   fcmToken: { type: String, required: true }, // Store user's FCM token
 });
 
